a68682ce28cafa631a08247f30170914cb1536bb
sergeypesegov <sergey.pesegov@yandex.ru>
2020-06-09 20:56:05 +0300

    yet another task for regular expression again

---
+++ 3.3.4.py
@@ -0,0 +1,18 @@
+'''Вам дана последовательность строк.
+Выведите строки, содержащие обратный слеш "\﻿".
+Sample Input:
+
+\w denotes word character
+No slashes here
+Sample Output:
+
+\w denotes word character
+'''
+import sys
+import re
+pattern = r'\\'
+
+for line in sys.stdin:
+    line = line.rstrip()
+    if re.findall(pattern, line):
+        print(line)
